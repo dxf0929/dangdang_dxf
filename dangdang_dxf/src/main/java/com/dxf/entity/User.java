eac20b9fd956ee3721a60e808e4b4721eb6a5bd5
dxf <dxf15835220929@163.com>
2020-10-17 22:12:12 +0800

    当当网环境搭建 1.0

---
+++ dangdang_dxf/src/main/java/com/dxf/entity/User.java
@@ -0,0 +1,8 @@
+package com.dxf.entity;
+
+/*
+ *创建日期：2020/10/17/22:11
+ *创建人：dxf
+ */
+public class User {
+}
---
+++ dangdang_dxf/src/test/java/com/dxf/dangdang_dxf/DangdangDxfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxf.dangdang_dxf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdangDxfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
